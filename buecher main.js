bc05286fa57d6ba34d7c9f14902aaa5f60dbf72f
heinefr <frederikheine37@gmail.com>
2022-06-09 12:55:21 +0200

    Initial commit

---
+++ buecher main.js
@@ -0,0 +1,23 @@
+class buch {
+    constructor(name , seiten , verleger) {
+
+        this.name = name
+        this.seiten = seiten
+        this.verleger = verleger
+    }
+    }
+
+
+    new buch ("Herr der Ringe" , "1600" , "Klett")
+    new buch ("Mein Kampf" , "800" , "Kampe")
+
+function verfuegbar(){
+
+
+}
+
+
+function verkauft(){
+
+
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+class moped {
+    constructor(model, kubik, geschwindigkeit)
+
+    this.model = model
+    this.kubik = kubik
+    this.geschwindigkeit = geschwindigkeit
+}
+
+function new_Moped() {
+ s51 = new moped("s51" , 60;)
+
+}
